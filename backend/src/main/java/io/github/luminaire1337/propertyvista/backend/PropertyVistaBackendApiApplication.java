fccc085a4e64e7081afae1bfdd7a8435022468c5
Luminaire1337 <16178449+Luminaire1337@users.noreply.github.com>
2025-10-03 19:31:05 +0200

    Add backend project generated by Spring Initialzr

---
+++ backend/src/main/java/io/github/luminaire1337/propertyvista/backend/PropertyVistaBackendApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.luminaire1337.propertyvista.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyVistaBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyVistaBackendApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/luminaire1337/propertyvista/backend/PropertyVistaBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.luminaire1337.propertyvista.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyVistaBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
