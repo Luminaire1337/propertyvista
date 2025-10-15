41b04589418ab57ab429522aabac9303842d82c4
Luminaire1337 <16178449+Luminaire1337@users.noreply.github.com>
2025-10-16 00:49:01 +0200

    Configure application properties and add Docker support

--- backend/src/main/java/io/github/luminaire1337/propertyvista/backend/PropertyVistaBackendApiApplication.java
+++ backend/src/main/java/io/github/luminaire1337/propertyvista/backend/PropertyVistaBackendApiApplication.java
@@ -1,13 +1,15 @@
 package io.github.luminaire1337.propertyvista.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class PropertyVistaBackendApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PropertyVistaBackendApiApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(PropertyVistaBackendApiApplication.class, args);
+    }
 
 }
