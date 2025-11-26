2a9d5b8e3df77a1da095e9517bf21b676f5f427b
Michał Łuczyński <16178449+Luminaire1337@users.noreply.github.com>
2025-11-26 02:16:19 +0100

    Upgrade Spring Boot to 4.0.0, fill up developer info in pom.xml, replace MySQL with MariaDB

--- backend/src/main/resources/db/migration/V3__create_verification_tokens_table.sql
+++ backend/src/main/resources/db/migration/V3__create_verification_tokens_table.sql
@@ -1,10 +1,9 @@
 CREATE TABLE verification_tokens
 (
-    id          BIGINT AUTO_INCREMENT NOT NULL,
-    token       VARCHAR(255)          NOT NULL,
-    user_id     BIGINT                NOT NULL,
-    expiry_date datetime              NOT NULL,
-    CONSTRAINT pk_verification_tokens PRIMARY KEY (id),
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    token       VARCHAR(255) NOT NULL,
+    user_id     BIGINT       NOT NULL,
+    expiry_date datetime     NOT NULL,
     CONSTRAINT uc_verification_tokens UNIQUE (token, user_id),
     CONSTRAINT fk_verification_tokens_on_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
