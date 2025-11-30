e8b8d0f0b2e110875e2bcb2a5d20a25f5c7be07f
Michał Łuczyński <16178449+Luminaire1337@users.noreply.github.com>
2025-11-30 01:31:56 +0100

    Make a mirror of Traefik's CORS settings only in backend's dev profile

--- backend/src/main/java/io/github/luminaire1337/propertyvista/backend/identity/internal/config/SecurityConfig.java
+++ backend/src/main/java/io/github/luminaire1337/propertyvista/backend/identity/internal/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,6 +32,12 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // Cors only gets applied in dev profile
+        if (corsConfigurationSource != null) {
+            http.cors(cors -> cors.configurationSource(corsConfigurationSource));
+        }
+        
         return http.build();
     }
 }
