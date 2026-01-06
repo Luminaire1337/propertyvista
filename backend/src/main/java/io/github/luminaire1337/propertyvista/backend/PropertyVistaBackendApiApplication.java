39f30a3221461677b3c7725801815403d008fbfe
Michał Łuczyński <16178449+Luminaire1337@users.noreply.github.com>
2026-01-06 01:26:12 +0100

    Remove unused Redis cache

--- backend/src/main/java/io/github/luminaire1337/propertyvista/backend/PropertyVistaBackendApiApplication.java
+++ backend/src/main/java/io/github/luminaire1337/propertyvista/backend/PropertyVistaBackendApiApplication.java
@@ -2,13 +2,11 @@ package io.github.luminaire1337.propertyvista.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
-@EnableCaching
 @SpringBootApplication
 public class PropertyVistaBackendApiApplication {
     public static void main(String[] args) {
